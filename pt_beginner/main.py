a1a6f30d0b933a9d7a24d3943fc02b24ed82a9ab
dhoby bumacod <dhoby.bumacod@globe.com.ph>
2023-06-09 21:18:52 +0800

    finished up to intermediate video #2

---
+++ pt_beginner/main.py
@@ -0,0 +1,12 @@
+# print("Magandang\tUmaga")
+# print("Magandang\nUmaga")
+# print('''
+#     Dhoby
+#     You
+#     Sexy
+#     Beast
+# ''')
+
+name,age = input().split()
+
+print(f'Hello! My name is {name.upper()} and I am {age} years old!! Uwu')
